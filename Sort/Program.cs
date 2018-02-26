ad3b6d3311a33a8f83b803d01d2d209496473310
Duran CHEN <duran.chen@nordanglia.com>
2018-02-26 19:31:56 +0800

    Shell Sort

--- Sort/Program.cs
+++ Sort/Program.cs
@@ -10,31 +10,34 @@ namespace Algorithms.Sort
         {
             int n = 10000;
             int[] a = SortTestHelper.generateRandomArray(n, 0, n);
-            int[] d = SortTestHelper.generateNearlyOrderedArray(n, 3);
+            int[] d = SortTestHelper.generateNearlyOrderedArray(n, 100);
 
             //SelectionSort.sort(a);
             //SortTestHelper.printArray(a);
             int[] b = new int[a.Length];
             int[] c = new int[a.Length];
             int[] e = new int[a.Length];
+            int[] f = new int[a.Length];
             Array.Copy(a, b, a.Length);
             Array.Copy(a, c, a.Length);
             Array.Copy(a, e, a.Length);
+            Array.Copy(a, f, a.Length);
 
             SortTestHelper.testSort("Selection Sort", SelectionSort.sort, a);
             SortTestHelper.testSort("Insertion Sort Basic", InsertionSort.sortBasic, b);
             SortTestHelper.testSort("Insertion Sort Advanced", InsertionSort.sortAdvanced, c);
             SortTestHelper.testSort("Insertion Sort Advanced Nearly Ordered", InsertionSort.sortAdvanced, d);
             SortTestHelper.testSort("Bubble Sort", BubbleSort.sort, e);
+            SortTestHelper.testSort("Shell Sort", ShellSort.sort, f);
 
-            //foreach (int i in e)
+            //foreach (int i in f)
             //{
             //    Console.WriteLine(i);
             //}
-            float[] f = { 3.1F, 2.2F, 3.2F, 1.1F };
-            InsertionSort.sortBasic(f);
+            float[] z = { 3.1F, 2.2F, 3.2F, 1.1F };
+            InsertionSort.sortBasic(z);
 
-            foreach (float i in f)
+            foreach (float i in z)
             {
                 Console.WriteLine(i);
             }
--- Sort/ShellSort.cs
+++ Sort/ShellSort.cs
@@ -10,6 +10,32 @@ namespace Algorithms.Sort
     {
         public static void sort<T>(T[] arr) where T : IComparable
         {
+            int n = arr.Length;
+
+            n = n / 2;
+
+            while (n > 0)
+            {
+                for (int i = n; i < arr.Length; i++)
+                {
+
+                    for (int j = i; j >= n; j = j - n)
+                    {
+                        if (arr[j - n].CompareTo(arr[j]) > 0)
+                        {
+                            SortTestHelper.swap(arr, j - n, j);
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+
+                n = n / 2;
+            }
+
+
 
         }
     }
