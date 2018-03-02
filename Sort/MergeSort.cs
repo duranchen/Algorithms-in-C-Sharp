cba2b09e1aa5e0819290f76b9d5f275cd8c0dbb5
Duran CHEN <duran.chen@gmail.com>
2018-03-02 09:18:36 +0800

    为归并排序增加注释

--- Sort/MergeSort.cs
+++ Sort/MergeSort.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,12 +13,14 @@ namespace Algorithms.Sort
 
         private static T[] aux;
 
+        //O(nlogn)时间复杂度
         public static void sort(T[] arr)
         {
             aux = new T[arr.Length];
             sort(arr, 0, arr.Length - 1);
         }
 
+        //递归调用归并排序，对arr[l...r]的范围排序。
         public static void sort(T[] arr, int l, int r)
         {
             if (r <= l)
@@ -30,36 +34,40 @@ namespace Algorithms.Sort
             merge(arr, l, mid, r);
 
         }
-
+        //将arr[l...mid]和arr[mid+1,r]进行归并
         public static void merge(T[] arr, int l, int mid, int r)
         {
-
+            // 将数组arr[l...r]复制辅助数组aux。
             for (int k = l; k <= r; k++)
             {
                 aux[k] = arr[k];
             }
 
 
+            //初始化，i指向左半部分起始位置l，j指向右半部分起始位置mid+1
             int i = l, j = mid + 1;
 
             for (int k = l; k <= r; k++)
             {
-
+                // 如左半部分元素已经处理完
                 if (i > mid)
                 {
                     arr[k] = aux[j];
                     j++;
                 }
+                // 如果右半部分元素已经处理完
                 else if (j > r)
                 {
                     arr[k] = aux[i];
                     i++;
                 }
+                // 如果左半部分i所指元素 < 右半部分j所指元素
                 else if (aux[i].CompareTo(aux[j]) < 0)
                 {
                     arr[k] = aux[i];
                     i++;
                 }
+                // 如果左半部分i所指元素 >= 右半部分j所指元素
                 else
                 {
                     arr[k] = aux[j];
