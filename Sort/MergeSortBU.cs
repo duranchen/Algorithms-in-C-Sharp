d78f16020b5631bcac06dd36e680aa1bdd592bcc
Duran CHEN <duran.chen@gmail.com>
2018-03-02 09:21:37 +0800

    自底向上归并排序-迭代

---
+++ Sort/MergeSortBU.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Sort
+{
+    class MergeSortBU<T> where T:IComparable
+    {
+        private static T[] aux;
+
+        //O(nlogn)时间复杂度
+        public static void sort(T[] arr)
+        {
+           
+            int n = arr.Length;
+            aux = new T[n];
+            for (int size = 1; size <n; size =size*2 )
+            {
+                for(int lo =0;lo<n-size;lo = lo +size*2)
+                {
+                    merge(arr, lo, lo + size - 1, Math.Min(lo + size * 2 - 1,n-1));
+                }
+            }
+           
+        }
+        //将arr[l...mid]和arr[mid+1,r]进行归并
+        public static void merge(T[] arr, int l, int mid, int r)
+        {
+            // 将数组arr[l...r]复制辅助数组aux。
+            for (int k = l; k <= r; k++)
+            {
+                aux[k] = arr[k];
+            }
+
+
+            //初始化，i指向左半部分起始位置l，j指向右半部分起始位置mid+1
+            int i = l, j = mid + 1;
+
+            for (int k = l; k <= r; k++)
+            {
+                // 如左半部分元素已经处理完
+                if (i > mid)
+                {
+                    arr[k] = aux[j];
+                    j++;
+                }
+                // 如果右半部分元素已经处理完
+                else if (j > r)
+                {
+                    arr[k] = aux[i];
+                    i++;
+                }
+                // 如果左半部分i所指元素 < 右半部分j所指元素
+                else if (aux[i].CompareTo(aux[j]) < 0)
+                {
+                    arr[k] = aux[i];
+                    i++;
+                }
+                // 如果左半部分i所指元素 >= 右半部分j所指元素
+                else
+                {
+                    arr[k] = aux[j];
+                    j++;
+                }
+
+
+            }
+
+
+        }
+
+    }
+}
--- Sort/Program.cs
+++ Sort/Program.cs
@@ -8,7 +8,7 @@ namespace Algorithms.Sort
 
         static void Main(string[] args)
         {
-            int n = 10000000;
+            int n = 1000000;
             int[] a = SortTestHelper.generateRandomArray(n, 0, n);
             int[] d = SortTestHelper.generateNearlyOrderedArray(n, 100);
 
@@ -19,40 +19,49 @@ namespace Algorithms.Sort
             int[] e = new int[a.Length];
             int[] f = new int[a.Length];
             int[] g = new int[a.Length];
+            int[] h = new int[a.Length];
+            int[] i = new int[a.Length];
+
 
             Array.Copy(a, b, a.Length);
             Array.Copy(a, c, a.Length);
             Array.Copy(a, e, a.Length);
             Array.Copy(a, f, a.Length);
-            Array.Copy(a, g, a.Length);
+            Array.Copy(a, h, a.Length);
+            Array.Copy(a, i, a.Length);
 
 
-         //   SortTestHelper.testSort("Selection Sort", SelectionSort.sort, a);
-         //   SortTestHelper.testSort("Insertion Sort Basic", InsertionSort.sortBasic, b);
+            //   SortTestHelper.testSort("Selection Sort", SelectionSort.sort, a);
+            //   SortTestHelper.testSort("Insertion Sort Basic", InsertionSort.sortBasic, b);
             SortTestHelper.testSort("Insertion Sort Advanced", InsertionSort.sortAdvanced, c);
             SortTestHelper.testSort("Insertion Sort Advanced Nearly Ordered", InsertionSort.sortAdvanced, d);
-         //   SortTestHelper.testSort("Bubble Sort", BubbleSort.sort, e);
-            SortTestHelper.testSort("Shell Sort", ShellSort.sort, f);
+           // SortTestHelper.testSort("Bubble Sort", BubbleSort.sort, e);
+            //SortTestHelper.testSort("Shell Sort", ShellSort.sort, f);
             SortTestHelper.testSort("Merge Sort", MergeSort<int>.sort, g);
+            SortTestHelper.testSort("Merge Sort Advanced", MergeSortAdvanced<int>.sort, h);
+            SortTestHelper.testSort("Merge Sort Buttom UP", MergeSortBU<int>.sort, i);
 
-            //foreach (int i in g )
+
+            //foreach (int i in e)
             //{
             //    Console.WriteLine(i);
             //}
+
+            Console.WriteLine("------------------------");
             float[] z = { 3.1F, 2.2F, 3.2F, 1.1F };
             InsertionSort.sortBasic(z);
 
-            foreach (float i in z)
+            foreach (float y in z)
             {
-                Console.WriteLine(i);
+                Console.WriteLine(y);
             }
 
             Student[] stu = { new Student("A", 10), new Student("C", 30), new Student("B", 20), new Student("D", 20) };
 
             SelectionSort.sort<Student>(stu);
-            foreach (Student i in stu)
+            foreach (Student y in stu)
             {
-                Console.WriteLine(i);
+                Console.WriteLine(y);
             }
 
         }
