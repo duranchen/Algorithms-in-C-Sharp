15fea2051ae0b6f56f46f879262dbdbc16568888
Duran CHEN <duran.chen@gmail.com>
2018-03-02 09:17:43 +0800

    delete wrong code

--- Sort/BubbleSort.cs
+++ Sort/BubbleSort.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Algorithms.Sort
 {
@@ -10,25 +6,17 @@ namespace Algorithms.Sort
     {
         public static void sort<T>(T[] arr) where T : IComparable
         {
-            //for(int i = 0; i<arr.Length;i++)
-            //{
-            //    for(int j = i+1; j<arr.Length;j++ )
-            //    {
-            //        if(arr[j].CompareTo(arr[i])<0)
-            //        {
-            //            SortTestHelper.swap(arr, j, i);
-            //        }
-            //    }
 
-            
-            for (int i = 0; i < arr.Length-1; i++)
+            //循环n-1次，只需冒泡n-1个元素
+            for (int i = 1; i <= arr.Length-1; i++)
             {
-               
-                for (int j = 1; j < arr.Length -i; j++)
+                //通过对arr[0..n-i]内的相邻元素依次进行比较并交换位置，使arr[0..n-i]中最大元素移动至arr[1,n-i]末端。
+                for (int j = 0; j < arr.Length -i; j++)
                 {
-                    if (arr[j - 1].CompareTo(arr[j]) > 0)
+                    //比较相邻元素，如果arr[j-1]>arr[j]则交换位置
+                    if (arr[j ].CompareTo(arr[j+1]) > 0)
                     {
-                        SortTestHelper.swap(arr, j - 1, j);
+                        SortTestHelper.swap(arr, j, j+1);
                     }
                 }
             
