1445bd15ce82f13d6e24a2a4935a050fc198a6a6
Duran CHEN <duran.chen@gmail.com>
2018-03-16 08:53:57 +0800

    MaxHeap init,size,isEmpty,insert.

---
+++ Heap/MaxHeap.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heap
+{
+    class MaxHeap<Item> where Item : IComparable
+    {
+        public Item[] items;
+        private int count;
+
+        public MaxHeap(int capacity)
+        {
+            items = new Item[capacity + 1];
+            count = 0;
+        }
+
+        public int size()
+        {
+            return count;
+        }
+
+        public bool isEmpty()
+        {
+            return count == 0 ? true : false;
+        }
+
+        public void insert(Item item)
+        {
+
+            items[++count] = item;
+            if (count > 1)
+            {
+
+                int i = count;
+
+                while (i > 1 && items[i / 2].CompareTo(items[i]) < 0)
+                {
+                    Item temp = items[i];
+                    items[i] = items[i / 2];
+                    items[i / 2] = temp;
+                    i = i / 2;
+                }
+            }
+
+        }
+
+    }
+}
---
+++ Heap/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MaxHeap<int> heap = new MaxHeap<int>(10);
+
+            heap.insert(30);
+            heap.insert(20);
+            heap.insert(10);
+            heap.insert(25);
+            heap.insert(100);
+
+
+            foreach (int item in heap.items )
+            {
+                Console.WriteLine(item);
+
+            }
+
+
+            Console.WriteLine(heap.size());
+        }
+    }
+}
