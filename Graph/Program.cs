6117ff722f167c91bf646246ac03939236c264b7
Duran CHEN <duran.chen@gmail.com>
2018-04-07 21:59:35 +0800

    从文件中创建图

--- Graph/Program.cs
+++ Graph/Program.cs
@@ -10,6 +10,20 @@ namespace Graph
     {
         static void Main(string[] args)
         {
+            string fileName = "C:\\Users\\duran_k6g7e3i\\Documents\\Visual Studio 2017\\Projects\\Algorithms\\Graph\\testG1.txt";
+
+            SparseGraph sg = new SparseGraph(13, false);
+            ReadGraph rsg = new ReadGraph(sg, fileName);
+
+            sg.print();
+            DenseGraph dg = new DenseGraph(13, false);
+
+            ReadGraph rdg = new ReadGraph(dg,fileName);
+
+            dg.print();
+
+
+
         }
     }
 }
---
+++ Graph/ReadGraph.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph
+{
+    class ReadGraph
+    {
+
+        public ReadGraph(Graph g,String fileName)
+        {
+            StreamReader sr = new StreamReader(fileName);
+
+            string firstLine = sr.ReadLine();
+
+            string[] VE = firstLine.Split(' ');
+
+            int V = int.Parse(VE[0]);
+            int E = int.Parse(VE[1]);
+
+            string line;
+            while((line = sr.ReadLine()) != null)
+            {
+                string[] vw = line.Split(' ');
+                int v = int.Parse(vw[0]);
+                int w = int.Parse(vw[1]);
+                g.addEdge(v, w);
+            }
+
+        }
+    }
+}
