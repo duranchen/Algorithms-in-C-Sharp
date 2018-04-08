d95dc094ff99a1ea297efb3d33152e1b99dd8dea
Duran CHEN <duran.chen@gmail.com>
2018-04-08 13:12:36 +0800

    图中两点间的路径

---
+++ Graph/Path.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph
+{
+    class Path
+    {
+        private Graph G;
+        private int source;
+        private bool[] visited;
+        private int[] from;
+
+        public Path(Graph graph,int s)
+        {
+            G = graph;
+            visited = new bool[G.V()];
+            from = new int[G.V()];
+            for(int i = 0; i<G.V();i++)
+            {
+                visited[i] = false;
+                from[i] = -1;
+            }
+            this.source = s;
+
+            dfs(s);
+        }
+
+        private void dfs(int v)
+        {
+            visited[v] = true;
+            List<int> adjV = (List<int>)G.adj(v);
+            foreach (int i in adjV)
+            {
+
+                if (visited[i] == false)
+                {
+                    from[i] = v;
+                    dfs(i);
+                }
+            }
+        }
+
+        public bool hasPath(int w)
+        {
+            return visited[w];
+        }
+
+        public List<int> path(int w)
+        {
+            int p = w;
+
+            List<int> vec = new List<int>();
+            Stack<int> stack = new Stack<int>();
+            
+            while(from[p] != -1)
+            {
+                stack.Push(p);
+                p = from[p];
+            }
+
+            if(stack.Count != 0 )
+            {
+                stack.Push(p);
+            }
+            
+
+            foreach(int v in stack)
+            {
+                vec.Add(v);
+            }
+
+            return vec;
+        }
+
+        public void printPath(int w)
+        {
+            List<int> path = new List<int>();
+
+            path = this.path(w);
+
+
+            Console.Write("{0:D} to {1:D} Path: ", source,w);
+            foreach (int i in path)
+            {
+                Console.Write("{0:D} ", i);
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
--- Graph/Program.cs
+++ Graph/Program.cs
@@ -10,17 +10,46 @@ namespace Graph
     {
         static void Main(string[] args)
         {
-            string fileName = "C:\\Users\\duran_k6g7e3i\\Documents\\Visual Studio 2017\\Projects\\Algorithms\\Graph\\testG1.txt";
+            string fileName1 = "C:\\Users\\duran_k6g7e3i\\Documents\\Visual Studio 2017\\Projects\\Algorithms\\Graph\\testG1.txt";
+            string fileName2 = "C:\\Users\\duran_k6g7e3i\\Documents\\Visual Studio 2017\\Projects\\Algorithms\\Graph\\testG2.txt";
 
-            SparseGraph sg = new SparseGraph(13, false);
-            ReadGraph rsg = new ReadGraph(sg, fileName);
 
-            sg.print();
-            DenseGraph dg = new DenseGraph(13, false);
+            SparseGraph sparseGraph = new SparseGraph(13, false);
+            ReadGraph rsg = new ReadGraph(sparseGraph, fileName1);
 
-            ReadGraph rdg = new ReadGraph(dg,fileName);
+            Components sparseGraphCount = new Components(sparseGraph);
 
-            dg.print();
+            Console.WriteLine("SparseGraph {0:D} components", sparseGraphCount.count());
+
+            int v = 0, w = 4;
+            Console.WriteLine("vertex {0:D} and vertex {1:D} connected: {2:D}",v,w,sparseGraphCount.isConnected(v, w));
+
+
+            Path sgPath = new Path(sparseGraph, v);
+
+            sgPath.printPath(w);
+
+            sparseGraph.print();
+
+
+            DenseGraph denseGraph = new DenseGraph(6, false);
+
+            ReadGraph rdg = new ReadGraph(denseGraph, fileName2);
+
+            Components denseGraphCount = new Components(denseGraph);
+
+            Console.WriteLine("DenseGraph {0:D} components", denseGraphCount.count());
+
+            Console.WriteLine("vertex {0:D} and vertex {1:D} connected: {2:D}", v, w, sparseGraphCount.isConnected(v, w));
+
+
+            Path dgGath = new Path(denseGraph, v);
+            dgGath.printPath(w);
+
+            denseGraph.print();
+
+
+
 
 
 
